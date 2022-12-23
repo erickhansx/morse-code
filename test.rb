957ec1d5ba634b93d08f72e8cf6ba1681d9171b3
erickhansx <erickhansx@gmail.com>
2022-12-23 15:18:35 -0600

    Fix linters

--- test.rb
+++ test.rb
@@ -1,58 +1,50 @@
+MORSE_CODE = {
+  a: '.-',
+  b: '-...',
+  c: '_.-.',
+  d: '-..',
+  e: '.',
+  f: '..-.',
+  g: '--.',
+  h: '....',
+  i: '..',
+  j: '.---',
+  k: '-.-',
+  l: '.-..',
+  m: '--',
+  n: '-.',
+  o: '---',
+  p: '.--.',
+  q: '--.-',
+  r: '.-.',
+  s: '...',
+  t: '-',
+  u: '..-',
+  v: '...-',
+  w: '.--',
+  x: '-..-',
+  y: '-.--',
+  z: '--..'
+}.freeze
 
-
-$morse_code = {
-   "a": ".-",
-   "b": "-...",
-   "c": "_.-.",
-   "d": "-..",
-   "e": ".",
-   "f": "..-.",
-   "g": "--.",
-   "h": "....",
-   "i": "..",
-   "j": ".---",
-   "k": "-.-",
-   "l": ".-..",
-   "m": "--",
-   "n": "-.",
-   "o": "---",
-   "p": ".--.",
-   "q": "--.-",
-   "r": ".-.",
-   "s": "...",
-   "t": "-",
-   "u": "..-",
-   "v": "...-",
-   "w": ".--",
-   "x": "-..-",
-   "y": "-.--",
-   "z": "--.."
-}
-
-
-def decode_char (char)
-  $morse_code.each do |key, code|
-    if code == char
-      return key.upcase
-    end
+def decode_char(char)
+  MORSE_CODE.each do |key, code|
+    return key.upcase if code == char
   end
 end
 
-decode_char("--")
+decode_char('--')
 
 def decode_word(word)
-  arr = word.split(" ")
+  arr = word.split
   arr_new = []
-  for i in 0..arr.length - 1
-  character = decode_char(arr[i])
-  arr_new.push(character)
-  end   
-  print arr_new.join("")
-  return arr_new
+  (0..arr.length - 1).each do |i|
+    character = decode_char(arr[i])
+    arr_new.push(character)
+  end
+  arr_new
 end
 
-decode_word("-- -.--")
+decode_word('-- -.--')
 
-def decode(str)
-  arr = str.split("   ")
-  
+
